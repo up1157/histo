b11b4b3e8ab6999792267f33ea6fe2a26524aebc
up1157 <up1157@naver.com>
2023-04-21 19:27:06 +0900

    Initial commit

---
+++ src/main/java/com/example/histo/HistoApplication.java
@@ -0,0 +1,13 @@
+package com.example.histo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HistoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/histo/HistoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.histo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
